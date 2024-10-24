8d45e9003404035dde4a0fe9b6802fd487227bda
Deepak Singhvi <dsinghvi@salesforce.com>
2024-10-24 22:22:33 +0530

    firebase deployment
    firebase deployment

--- README.md
+++ README.md
@@ -8,3 +8,20 @@
 flutter run or 
 flutter run - chrome
 ```
+
+## To deploy to firebase
+
+```
+flutter build web
+```
+content of public/index.hmml to be copied to build/web/index.html
+
+```
+firebase deploy
+```
+p.s. firebase.json has been updated for the public folder to build/web
+"public": "build/web",
+
+Application can be access at
+
+https://ecommerce-catalog-9547a.web.app/
--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -17,7 +17,12 @@ class ProductDetailScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Image.network(product.imageUrl),
+            Center(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.network(product.imageUrl, height: 250, fit: BoxFit.cover),
+              ),
+            ),
             SizedBox(height: 10),
             Text(
               product.title,
@@ -25,10 +30,20 @@ class ProductDetailScreen extends StatelessWidget {
             ),
             SizedBox(height: 10),
             Text(
-              '\$${product.price.toStringAsFixed(2)}',
+              'Product ID: ${product.id}',
+              style: TextStyle(fontSize: 16, color: Colors.grey[600]),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Price: \$${product.price.toStringAsFixed(2)}',
               style: TextStyle(fontSize: 20, color: Colors.green),
             ),
             SizedBox(height: 10),
+            Text(
+              'Description:',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 5),
             Text(
               product.description,
               style: TextStyle(fontSize: 16),
